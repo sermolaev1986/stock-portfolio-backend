ff7ed27a9479d318f1d74da9c19a79c3db8c039f
Sergei Ermolaev <sergei.ermolaev@t-mobile.at>
2022-03-14 12:20:11 +0100

    added new transactions

---
+++ src/main/resources/db/changelog/V1.0.0/V22.03.14.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+--changeset ermolaevao:V1.0.0
+
+INSERT INTO transaction (symbol, argument, operator, price, owner, date)
+VALUES
+-- HSBC MSCI Russia
+('H4ZM.DE', 391, '+', 4.65, 'Sergei', '2022-02-28T15:23:48.388'),
+-- Vanguard USD Corporate Bond
+('VUCP', 36.085, '+', 27, 'Sergei', '2022-03-01T15:23:48.388'),
+('VUCP', 36.085, '+', 38, 'Olga', '2022-03-01T15:23:48.388')
