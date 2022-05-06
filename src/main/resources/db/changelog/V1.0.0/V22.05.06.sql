88101c4c63b916bd5961245d91dbb208dcd18270
Sergei Ermolaev <sergei.ermolaev@t-mobile.at>
2022-05-06 14:34:35 +0200

    added new transactions

---
+++ src/main/resources/db/changelog/V1.0.0/V22.05.06.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+--changeset ermolaevs:V1.0.0
+
+INSERT INTO transaction (symbol, argument, operator, price, owner, date)
+VALUES
+-- Sergei Easterly
+('SRB', 14, '+', 73.32, 'Sergei', '2022-04-21T15:23:48.388'),
+('VGWD', 12.93, '+', 58.01, 'Sergei', '2022-04-21T15:23:48.388'),
+('VGWD', 12.93, '+', 58.01, 'Olga', '2022-04-21T15:23:48.388')
+
